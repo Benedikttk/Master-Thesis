c4cf59898ec817726d9fe0061a2cd9649147e211
benedikttk <benja2204@live.dk>
2025-03-02 22:52:32 +0100

    Remove deleted files from repository

--- Optimized_scripts/config.py
+++
@@ -1,8 +0,0 @@
-# Constants
-EFFECTIVE_LENGTH = 3.15e9  # angstrom (315 mm)
-ANODE_1_LENGTH = EFFECTIVE_LENGTH / 3  # delta E
-ANODE_2_LENGTH = 2 * EFFECTIVE_LENGTH / 3  # E_res
-
-# File paths
-DATA_DIR = r"C:\Users\benja\Desktop\Speciale\Data"
-BILLEDER_PATH = r"C:\Users\benja\Desktop\Speciale\Billeder"
