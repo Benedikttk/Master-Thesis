2c70dd4d5df2a278d6ccab25ca597c2f3262cc32
benedikttk <benja2204@live.dk>
2025-03-03 16:50:43 +0100

    Cleanup

--- Scripts/AARAMS_dE_Efinal.py
+++ Scripts/AARAMS_dE_Efinal.py
@@ -6,7 +6,7 @@ import seaborn as sns
 from Functions import FileCheck, deltE_Efinal
 
 # 
-Filepath = r'C:\Users\benja\Desktop\Speciale\Master-Thesis\Data\Første måling af Be10\2025_01_16_Benedikt\2025_01_16_Benedikt'
+Filepath = r'C:\Users\benja\Desktop\Speciale\Data\Første måling af Be10\2025_01_16_Benedikt\2025_01_16_Benedikt'
 Subject = "[CDAT0"
 
 files = [i for i in FileCheck(filepath=Filepath, endswith=".txt.mpa")]
