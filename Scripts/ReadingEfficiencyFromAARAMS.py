ef5257f0fdd9fd139cf7d72718c00ea09eb57135
benedikttk <benja2204@live.dk>
2025-03-03 16:51:40 +0100

    optimized with functions

--- Scripts/ReadingEfficiencyFromAARAMS.py
+++ Scripts/ReadingEfficiencyFromAARAMS.py
@@ -11,7 +11,7 @@ list_of_files = get_txt_files(filepath, ".txt")
 if not list_of_files:
     print("No .txt files found.")
 else:
-    data_lines = read_block_data(filepath + "\\" + list_of_files[0])
+    data_lines = read_block_data(filepath + "\\" + list_of_files[1])
     
     column_names = [
         "Blk", "10Becnts", "Totalcnts", "10Bcnts", "cnts", "cnts",
@@ -45,3 +45,19 @@ else:
     
     print(f"The ratio of Be10/Be9 is {R_n} ± {R_n_uncertainty}")
     print(f"The isotropic ratio efficiency is {round(iso_eff, 3)} ± {round(iso_eff_uncertainty, 3)} %")
+    
+    
+  # Set Be10 counts to 1404.9
+avg_Be10cnts = 1404.9
+
+# Make sure Be9cnts is correctly calculated
+Be9cnts = calculate_Be9_ions(Be9_current, detector_live_time)
+
+# Now call the function to calculate the ratio and efficiency
+R_n, R_n_uncertainty, iso_eff, iso_eff_uncertainty = calculate_ratio_and_efficiency(
+    avg_Be10cnts, Be9cnts, std_Be10cnts, 27.1e-12, 0.3e-12
+)
+
+# Output the results
+print(f"The ratio of Be10/Be9 is {R_n} ± {R_n_uncertainty}")
+print(f"The isotropic ratio efficiency is {round(iso_eff, 3)} ± {round(iso_eff_uncertainty, 3)} %")
